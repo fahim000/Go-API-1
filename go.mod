a9c2de3bac54b42f8add583e200c381399f32826
fahim000 <fahimmontasir74@gmail.com>
2025-03-26 16:29:34 +0600

    Fixed formhandler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fahim000/go-web-server
+
+go 1.23.7
--- main.go
+++ main.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("ParseForm() error: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Email = %s\n", email)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
@@ -24,8 +38,8 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Printf("Starting server at port 8000\n")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
